5637e30cfb31c1f648acf422a65a4d9db88ebf7d
Kate Kremer <katrinakremer@gmail.com>
2019-08-20 11:05:58 -0400

    create main server.js file and install express and mongoose

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors')
+
+mongoose.connect('mongodb:localhost/blocked', {useNewURLParser: true});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+const mainRoutes = require('./routes/main')
+
+app.use(mainRoutes)
+
+app.listen(8000, () => {
+  console.log('Node.js is listening on port ' + 8000)
+})
