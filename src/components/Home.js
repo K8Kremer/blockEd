ddad6efcdeb3b6f57a03f7e310fb86b4a615c1b6
Kate Kremer <katrinakremer@gmail.com>
2019-08-22 00:17:34 -0400

    add background image for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './components.css';
 
 function Home () {
   return(
-    <div> home </div>
+    <>
+    <div className='view jarallax hero-image' data-jarallax='{"speed":0.2}'> 
+    <img className='backpic' src="https://cdn.pixabay.com/photo/2019/01/25/19/33/degree-3955169__340.jpg"/>
+    <div className='img-overlay d-flex justify-content-center align-items-center'>
+    <h1>Student-Owned Credentials</h1>
+
+    </div>
+    
+    </div>
+    </>
   )
 }
 
