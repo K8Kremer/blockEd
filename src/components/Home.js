9d47ca63791e3b0cfea45f36e053b22c3ad6cca1
Kate Kremer <katrinakremer@gmail.com>
2019-08-22 12:06:05 -0400

    create working directory buttons on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,18 +7,18 @@ import { faLink, faPaperPlane, faUserCheck } from '@fortawesome/free-solid-svg-i
 function Home () {
   return(
     <>
-      <div className='view jarallax hero-image' data-jarallax='{"speed":0.2}'> 
-      <img className='backpic' src="https://cdn.pixabay.com/photo/2019/01/25/19/33/degree-3955169__340.jpg"/>
+      <div className='hero-image'> 
+      <img className='backpic img-fluid' src="https://cdn.pixabay.com/photo/2019/01/25/19/33/degree-3955169__340.jpg"/>
         <div className='img-overlay justify-content-center align-items-center'>
         <div className='row'>
         <h1 className='intro-header'>Student-Owned Credentials</h1>
         </div>
           <div className='row'>
             <div className='col-sm-6 align-items-center'>
-        <button className='issue-link-button btn btn-primary btn-block shadow mb-5 bg-white rounded' id='issue-button'>Issue</button>
+        <a href='/issue' className='issue-link-button btn btn-primary btn-block shadow mb-5 bg-white rounded' id='issue-button'>Issue</a>
         </div>
         <div className='col-sm-6'>
-        <button className='issue-link-button btn btn-primary btn-block' id='verify-button'>Verify</button>
+        <a href='verify' className='issue-link-button btn btn-primary btn-block' id='verify-button'>Verify</a>
         </div>
         </div>
         </div>
@@ -28,21 +28,21 @@ function Home () {
         <h1 className='expl-title'>How it Works</h1>
         </div>
         <div className='card-group text-center'>
-          <div className='card hoverable-card'>
+          <div className='card hoverable-card' id='remove-border1'>
             <div className='card-body expl-text'>
             <FontAwesomeIcon icon={faLink } className='intro-icon'/>
             <div className='card-title'>Issue a Transcript</div>
             <div className='card-text'>Follow a simple two step process to record the transcript on the blockchain and thus transfer ownership to the student.</div>
             </div>
           </div>
-          <div className='card hoverable-card'>
+          <div className='card hoverable-card' id='remove-border1'>
             <div className='card-body expl-text'>
             <FontAwesomeIcon icon={faPaperPlane} className='intro-icon'/>
             <div className='card-title'>Deliver</div>
             <div className='card-text'>Follow a simple two step process to record the transcript on the blockchain and thus transfer ownership to the student.</div>
             </div>
           </div>
-          <div className='card hoverable-card'>
+          <div className='card hoverable-card' id='remove-border1'>
             <div className='card-body expl-text'>
             <FontAwesomeIcon icon={faUserCheck} className='intro-icon'/>
             <div className='card-title'>Verify</div>
